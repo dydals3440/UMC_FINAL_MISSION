6988395c60fcf766e38fc2919111b3278f826eb0
김용민 <dydals3440@gmail.com>
2024-11-26 14:15:18 +0900

    feat: posts관련 스웨거 작성

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -7,15 +7,16 @@ import {
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { ApiFile } from './decorators/api-file.decorator';
-import { ApiCookieAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiCookieAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('common')
 @Controller('common')
 export class CommonController {
   @Post('image')
   @ApiOperation({
     summary: '사전 이미지 업로드',
     description:
-      '이미지를 미리 업로드하여, URL을 반환 받습니다. 1개의 이미지만 가능하며 png 파일만 가능합니다, 반환 받은 URL을 통해, 추후 게시글 작성시 imageUrl에 넘겨주시면 됩니다. public temp 폴더에 임시 저장했다가, 추후 게시글 업로드에 해당 imageUrl을 사용시, image 폴더로 이동합니다. (image 폴더로 이동해야, 실제 화면에 보임)',
+      '이미지를 미리 업로드하여, URL을 반환 받습니다. 1개의 이미지만 가능하며 png 파일만 가능합니다, 반환 받은 URL을 통해, 추후 게시글 작성시 imageUrl에 넘겨주시면 됩니다. public temp 폴더에 임시 저장했다가, 추후 게시글 업로드에 해당 imageUrl을 사용시, image 폴더로 이동합니다. (image 폴더로 이미지가 이동해야, 실제 화면에 보임)',
   })
   @ApiCookieAuth()
   @UseGuards(JwtAuthGuard)
--- src/common/dto/cursor-pagination.dto.ts
+++ src/common/dto/cursor-pagination.dto.ts
@@ -1,22 +1,39 @@
 import { IsArray, IsInt, IsOptional, IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CursorPaginationDto {
   @IsString()
   @IsOptional()
   // id_52, likeCount_20
+  @ApiProperty({
+    type: 'string',
+    description:
+      '커서 페이지네이션의 커서를 전달해주면 됩니다. nextCursor를 전달해주면 됩니다.',
+    example: 1,
+    required: false,
+  })
   cursor?: string;
 
-  @IsArray()
-  @IsString({
-    each: true,
+  @ApiProperty({
+    type: [String],
+    name: 'order[]',
+    description:
+      '정렬 순서를 지정합니다. 배열 형식으로 전달하세요. 예: ?order[]=id_DESC&order[]=likeCount_DESC (id_ASC, id_DESC, likeCount_ASC, likeCount_DESC)',
+    example: ['id_DESC', 'likeCount_DESC'],
+    required: false,
   })
+  @IsArray()
+  @IsString({ each: true })
   @IsOptional()
-  // id_ASC id_DESC
-  // [id_DESC, likeCount_DESC]
-  // 기본값은 내림차순
   order: string[] = ['id_DESC'];
 
+  @ApiProperty({
+    type: 'number',
+    description: '몇개의 게시글을 가져올지 정합니다. 기본값은 2입니다.',
+    example: 2,
+    required: false,
+  })
   @IsInt()
   @Transform(({ value }) => {
     // value가 문자열일 때만 parseInt로 변환
